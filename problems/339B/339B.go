148ca86209cc9aa320b8feef959e9f5e56dce5ac
Herbie <hjrand1005@gmail.com>
2023-07-08 00:36:20 -0400

    339B

---
+++ problems/339B/339B.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func solve(n int, tasks []int) int {
+	pos := 1
+	time := 0
+	for _, t := range tasks {
+		if pos > t {
+			time += n - (pos - t)
+		} else if pos < t {
+			time += t - pos
+		}
+		pos = t
+	}
+	return time
+}
+
+func stringToIntSlice(s string) []int {
+	nums := strings.Split(s, " ")
+	ints := make([]int, len(nums))
+	for i, num := range nums {
+		n, err := strconv.Atoi(num)
+		if err != nil {
+			log.Fatalf("failed to parse int: %v\n", err)
+		}
+		ints[i] = n
+	}
+	return ints
+}
+
+func stringSliceToIntSlice(nums []string) []int {
+	ints := make([]int, len(nums))
+	for i, num := range nums {
+		n, err := strconv.Atoi(num)
+		if err != nil {
+			log.Fatalf("failed to parse int: %v\n", err)
+		}
+		ints[i] = n
+	}
+	return ints
+}
+
+func readString(r *bufio.Reader) string {
+	s, err := r.ReadString('\n')
+	if err != nil {
+		log.Fatalf("failed to read string: %v\n", err)
+	}
+	s = strings.ReplaceAll(s, "\r", "")
+	s = strings.ReplaceAll(s, "\n", "")
+	return s
+}
+
+func readInt(r *bufio.Reader) int {
+	s := readString(r)
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatalf("failed to parse int: %v\n", err)
+	}
+	return n
+}
+
+func readStringSlice(r *bufio.Reader) []string {
+	s := readString(r)
+	return strings.Split(s, " ")
+}
+
+func readIntSlice(r *bufio.Reader) []int {
+	nums := readStringSlice(r)
+	return stringSliceToIntSlice(nums)
+}
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	nm := readIntSlice(r)
+	n := nm[0]
+	tasks := readIntSlice(r)
+
+	result := solve(n, tasks)
+	w.WriteString(fmt.Sprintf("%v\n", result))
+}
